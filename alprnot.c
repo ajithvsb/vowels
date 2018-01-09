33ab1f4c7ff24c34453c440ca49fec09bee73fe0
ajithvsb <31505680+ajithvsb@users.noreply.github.com>
2018-01-09 10:25:28 +0000

    Create alprnot.c

---
+++ alprnot.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+    char c;
+    clrscr();
+
+   
+    printf("Enter any character: ");
+   
+    scanf("%c",&c);
+
+    if( (c>='a' && c<='z') || (c>='A' && c<='Z'))
+        printf("The entered character %c is an Alphabet",c);
+    else
+        printf("The entered character %c is not an Alphabet",c);
+
+    return 0;
+}
