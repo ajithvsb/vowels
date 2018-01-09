6530011f2749df3098b9891533594cdd1aaa67d8
ajithvsb <31505680+ajithvsb@users.noreply.github.com>
2018-01-09 10:46:26 +0000

    Create leapyrnot.c

---
+++ leapyrnot.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+    int y;
+
+    printf("Enter a year: ");
+    scanf("%d",&y);
+
+    if(y%4 == 0)
+    {
+        if( y%100 == 0)
+        {
+           
+            if ( y%400 == 0)
+                printf("%d is a leap year.", year);
+            else
+                printf("%d is not a leap year.", year);
+        }
+        else
+            printf("%d is a leap year.", year );
+    }
+    else
+        printf("%d is not a leap year.", year);
+    
+    return 0;
+}
