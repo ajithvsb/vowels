2af8b14fdeccebb638dbc3e9c97ff93f8da9a3cb
ajithvsb <31505680+ajithvsb@users.noreply.github.com>
2018-01-18 10:32:57 +0000

    Create sum.c

---
+++ sum.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int i,sum=0,num;
+printf("\n enter an integer number")
+scanf("%d",&num);
+for(i=1;i<=num;i++)
+{
+sum=sum+i;
+}
+printf("%d is an natural number");
+return 0;
+}
