a9d306b5dee89aaf98182ba2dd7b2651b586e893
ajithvsb <31505680+ajithvsb@users.noreply.github.com>
2018-03-20 16:56:45 +0530

    Create 39.c

---
+++ 39.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include<string.h>
+int main(void) {
+	
+int a,i,c=1,d=0;
+for(i=1;i<=a;i++)
+{
+c=c*2;
+if(c==a)
+{
+d=c; break;
+}
+else{
+d=c;}}
+if(a==d)
+{
+printf("yes");
+}
+else
+{
+printf("no");
+}
+return 0;
+}
